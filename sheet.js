83e29a43fc4a89eb23350a41985d2f2898485d5c
Yashvardhan7404 <155538903+Yashvardhan7404@users.noreply.github.com>
2024-02-28 15:16:59 +0500

    Update sheet.js

--- sheet.js
+++ sheet.js
@@ -58,3 +58,38 @@ left:100, behavior:'smooth'
     });
 }
 
+setInterval(banner_slide, 6000);
+
+let i=1;
+
+function banner_slide(){
+
+    if(i%2==0)
+    {
+        document.getElementById('bimg_1').style.display='none';
+        document.getElementById('bimg_2').style.display='block';
+        document.getElementById('bimg_2').style.transition='all 0.5s ease-in';
+        document.getElementById('bimg_1').style.transition='all 0.5s ease-in';
+        document.getElementById('banner_heading').style.color='black';
+        document.getElementById('banner_para').style.color='black';
+        document.getElementById('banner_link').style.color='black';
+        document.getElementById('banner_link').style.border='1px solid black';
+
+
+        
+        i++;
+    }
+
+    else{
+        document.getElementById('bimg_1').style.display='block';
+        document.getElementById('bimg_2').style.display='none';
+        document.getElementById('bimg_2').style.transition='all 0.5s ease-in';
+        document.getElementById('bimg_1').style.transition='all 0.5s ease-in';
+        document.getElementById('banner_heading').style.color='white';
+        document.getElementById('banner_para').style.color='white';
+        document.getElementById('banner_link').style.color='white';
+        document.getElementById('banner_link').style.border='1px solid white';
+
+        i++;
+    }
+}
